129e75264e3beb22cdbbee28bcf4875c964f62ed
Domi <dominikabobik12@gmail.com>
2022-04-06 15:49:43 -0400

    create accounts

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const sleep = (ms = 1000) => new Promise((r) => setTimeout(r, ms));
 
 console.log(figlet.textSync('Welcome to DBank!', {}));
 
+
 const hasAccount =  await inquirer.prompt({
     name: 'askAccount',
     type: 'list',
@@ -29,6 +30,27 @@ if (hasAccount.askAccount.localeCompare("Yes") == 0) {
 }
 else {
     console.log('Create account');
+    createAccount();
+}
+
+async function promptUser() {
+    const hasAccount =  await inquirer.prompt({
+        name: 'askAccount',
+        type: 'list',
+        message: 'Do you have an account?\n',
+        choices: [
+            'Yes',
+            'No',
+        ],
+    });
+    
+    if (hasAccount.askAccount.localeCompare("Yes") == 0) {
+        login();
+    }
+    else {
+        console.log('Create account');
+        createAccount();
+    }
 }
 
 async function login(){
@@ -58,4 +80,37 @@ async function login(){
     else {
         spinner.error({ text: `Invalid credentials` });
     }
+}
+
+async function createAccount(){
+
+    let newUser;
+    let newPassword;
+
+    while (1){
+        const userInput = await inquirer.prompt({
+            name: 'askUsername',
+            type: 'input',
+            message: 'Enter your new username',
+        });
+    
+        newUser = userInput.askUsername;
+        if (map.get(newUser) != null )
+        {
+            console.log('Username already exists, please pick different one\n');
+        }
+        else { break; }
+    }
+
+    const passwordInput = await inquirer.prompt({
+        name: 'askPassword',
+        type: 'input',
+        message: 'Enter your new password',
+    });
+
+    newPassword = passwordInput.askPassword;
+    
+    map.set(newUser, newPassword);
+    console.log("Your account has been sucessfully created\n");
+    promptUser();
 }
